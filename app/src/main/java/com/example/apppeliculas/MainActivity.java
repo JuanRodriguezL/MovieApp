b8b6ac6fc987e3f7d4ee5dcb448255aa2f2bc157
JuanRodriguezL <juangrl2143@gmail.com>
2023-01-14 14:08:48 -0500

    ajuste de scroll y diseño vista principal

--- app/src/main/java/com/example/apppeliculas/MainActivity.java
+++ app/src/main/java/com/example/apppeliculas/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.apppeliculas;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,7 +19,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window w = getWindow();
+            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
         setContentView(R.layout.activity_main);
         BottomNavigationView navigation = findViewById(R.id.navigationOption);
         navigation.setOnItemSelectedListener(item -> {
